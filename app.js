b0136f084ba3de7ec9d181cec03bfbf79c45ee83
AlElenaV <56462583+AlElenaV@users.noreply.github.com>
2020-06-18 17:03:49 +1000

    Add files via upload
    create app with http://api.icndb.com/jokes/random/

---
+++ app.js
@@ -0,0 +1,26 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e){
+  const number=document.querySelector('input[type="number"]').value;
+
+  const xhr=new XMLHttpRequest(); //1
+ xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`,true); //2
+  xhr.onload=function(){
+    if(this.status===200){ 
+    const response=JSON.parse(this.responseText);
+    let output=''; 
+    if(response.type==='success'){
+      response.value.forEach(function(joke){
+        output+=`<li>${joke.joke}</li>`;
+      });
+    }else{
+      output+='<li>Something went wrong</li>';
+    }
+    document.querySelector('.jokes').innerHTML=output;
+    }
+  }
+
+  xhr.send(); //3
+
+  e.preventDefault();
+}
